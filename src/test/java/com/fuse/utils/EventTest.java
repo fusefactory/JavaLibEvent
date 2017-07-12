ed6789af25e3f2c30a06fdf1ecab99cb43a78393
shortnotion (mark) <shortnotion@gmail.com>
2017-07-12 07:27:33 +0200

    test update

--- src/test/java/com/fuse/utils/EventTest.java
+++ src/test/java/com/fuse/utils/EventTest.java
@@ -2,111 +2,81 @@ package com.fuse.utils;
 
 import java.util.function.Consumer;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
-import com.fuse.utils.Event;
+public class EventTest {
 
-/**
- * Unit test for com.fuse.utils.Event.
- */
-public class EventTest extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public EventTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( EventTest.class );
-    }
-
-    /**
-     * Test Logic
-     */
     private String result;
     private Event<String> event;
     private Object owner;
 
-    public void testApp()
-    {
-     
-      {
-        event = new Event<String>();
-        result = "";
+    @Test public void addListener_addOnceListener_trigger_removeListeners(){
+      event = new Event<String>();
+      result = "";
 
-        // trigger without listener(s)
-        event.trigger("trigger1");
+      // trigger without listener(s)
+      event.trigger("trigger1");
 
-        // add listener
-        Consumer<String> consumer = (String value) -> {
-        	result = result + value;
-        };
+      // add listener
+      Consumer<String> consumer = (String value) -> {
+      	result = result + value;
+      };
 
-        event.addListener(consumer);
+      event.addListener(consumer);
 
-        // trigger listener
-        event.trigger("trigger2");
-        assertEquals(result, "trigger2");
+      // trigger listener
+      event.trigger("trigger2");
+      assertEquals(result, "trigger2");
 
-        // remove listener
-        event.removeListener(consumer);
+      // remove listener
+      event.removeListener(consumer);
 
-        // trigger without listener(s)
-        event.trigger("trigger3");
-        assertEquals(result, "trigger2");
+      // trigger without listener(s)
+      event.trigger("trigger3");
+      assertEquals(result, "trigger2");
 
-        // add new listener
-        event.addListener((String val) -> {
-        	result += " -> " + val;
-        }, this);
+      // add new listener
+      event.addListener((String val) -> {
+      	result += " -> " + val;
+      }, this);
 
-        // trigger with new listener
-        event.trigger("trigger4");
-        assertEquals(result, "trigger2 -> trigger4");
+      // trigger with new listener
+      event.trigger("trigger4");
+      assertEquals(result, "trigger2 -> trigger4");
 
-        // remove new listener by owner
-        event.removeListeners(this);
+      // remove new listener by owner
+      event.removeListeners(this);
 
-        // trigger without listener(s)
-        event.trigger("trigger5");
-        assertEquals(result, "trigger2 -> trigger4");
+      // trigger without listener(s)
+      event.trigger("trigger5");
+      assertEquals(result, "trigger2 -> trigger4");
 
-        // add "once listener" (listener will auto-remove after first invocation)
-        event.addOnceListener((String value) -> {
-        	result += " # " + value;
-        });
+      // add "once listener" (listener will auto-remove after first invocation)
+      event.addOnceListener((String value) -> {
+      	result += " # " + value;
+      });
 
-        // trigger twice, only first one the listener is called
-        event.trigger("trigger6");
-        event.trigger("trigger7");
-        assertEquals(result, "trigger2 -> trigger4 # trigger6");
+      // trigger twice, only first one the listener is called
+      event.trigger("trigger6");
+      event.trigger("trigger7");
+      assertEquals(result, "trigger2 -> trigger4 # trigger6");
 
-        Object owner = this;
-        // register listener that tries to modify to event while it's triggering
-        event.addListener((String val) -> {
-        	event.addListener((String v) -> {}, owner);
-        	result += " _ " + val + " (after add: "+ Integer.toString(event.size());
-        	event.removeListeners(owner);
-        	result += ", after remove: "+ Integer.toString(event.size()) + ")";
-        }, this);
+      Object owner = this;
+      // register listener that tries to modify to event while it's triggering
+      event.addListener((String val) -> {
+      	event.addListener((String v) -> {}, owner);
+      	result += " _ " + val + " (after add: "+ Integer.toString(event.size());
+      	event.removeListeners(owner);
+      	result += ", after remove: "+ Integer.toString(event.size()) + ")";
+      }, this);
 
-        assertEquals(event.size(), 1);
-        event.trigger("trigger8");
-        assertEquals(event.size(), 0);
-        assertEquals(result, "trigger2 -> trigger4 # trigger6 _ trigger8 (after add: 1, after remove: 1)");
-      }
-      {
+      assertEquals(event.size(), 1);
+      event.trigger("trigger8");
+      assertEquals(event.size(), 0);
+      assertEquals(result, "trigger2 -> trigger4 # trigger6 _ trigger8 (after add: 1, after remove: 1)");
+    }
+    @Test public void forward_stopForward(){
         Event<String> e1 = new Event<String>();
         Event<String> e2 = new Event<String>();
 
@@ -129,6 +99,5 @@ public class EventTest extends TestCase
         e2.stopForward(e1);
         e1.trigger("trigger4");
         assertEquals(result, "trigger2trigger3");
-      }
     }
 }
