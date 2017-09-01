b491b8acf1396744d2c63aa4b0200b59b6657261
mark <mark.korput@gmail.com>
2017-09-02 00:09:42 +0200

    benchmark test

--- src/test/java/com/fuse/utils/EventTest.java
+++ src/test/java/com/fuse/utils/EventTest.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.function.Consumer;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class EventTest {
@@ -218,5 +219,32 @@ public class EventTest {
 
 		assertEquals(event.size(), 0);
 		assertEquals(forwardSource.size(), 0);
+	}
+
+	@Test public void benchmark(){
+		Event<String> root = new Event<>();
+		Event<String> nest1 = new Event<>();
+		Event<String> nest2 = new Event<>();
+
+		nest2.addListener((String val) -> {});
+
+		nest1.addListener((String val) -> {
+			for(int i=0; i<100; i++)
+				nest2.trigger("foo");
+		});
+
+		root.addListener((String val) -> {
+			for(int i=0; i<100; i++)
+				nest1.trigger("bar");
+		});
+
+		long t1 = System.currentTimeMillis();
+
+		for(int i=0; i<100; i++)
+			root.trigger("foobar");
+
+		long t2 = System.currentTimeMillis();
+		assertTrue((t2-t1) < 120);
+		assertTrue((t2-t1) > 80);
 	}
 }
